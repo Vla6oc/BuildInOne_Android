524b983802e45873628a92b20d5c7eeb20a015fd
Vla6oc <Vla6oc@users.noreply.github.com>
2021-04-13 10:36:32 +0300

    ver1

--- app/src/main/java/com/example/cp/intro.java
+++ app/src/main/java/com/example/cp/intro.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -20,6 +19,9 @@ public class intro extends Activity {
     private ImageView logo1;
     private ImageView logo2;
     private ImageView logo3;
+    private ImageView back1;
+    private ImageView back2;
+    private ImageView back3;
     private ConstraintLayout cl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +31,21 @@ public class intro extends Activity {
         logo1 = (ImageView) findViewById(R.id.logo1);
         logo2 = (ImageView) findViewById(R.id.logo2);
         logo3 = (ImageView) findViewById(R.id.logo3);
+        back1 = (ImageView) findViewById(R.id.l1);
+        back2 = (ImageView) findViewById(R.id.l2);
+        back3 = (ImageView) findViewById(R.id.l3);
         cl = (ConstraintLayout) findViewById(R.id.clc);
         Animation lo1 = AnimationUtils.loadAnimation(this, R.anim.log1);
         Animation lo2 = AnimationUtils.loadAnimation(this, R.anim.log2);
         Animation lo3 = AnimationUtils.loadAnimation(this, R.anim.log3);
         getActionBar().hide();
+        back1.startAnimation(lo1);
         logo1.startAnimation(lo1);
+        back2.startAnimation(lo2);
         logo2.startAnimation(lo2);
+        back3.startAnimation(lo3);
         logo3.startAnimation(lo3);
-            new CountDownTimer(2500, 500) {
+            new CountDownTimer(3000, 500) {
                 @Override
                 public void onTick(long l) {
 
