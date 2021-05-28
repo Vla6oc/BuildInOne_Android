9327c697a2b0b18262f7d61a0de0f0ec0a08d8e0
Vla6oc <Vla6oc@users.noreply.github.com>
2021-05-28 23:25:15 +0300

    upd_28.05.2021

--- app/src/main/java/com/example/cp/intro.java
+++ app/src/main/java/com/example/cp/intro.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -15,12 +16,19 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class intro extends Activity {
-    private FrameLayout logo;
     private ImageView logo1;
     private ImageView logo2;
     private ImageView logo3;
     private ImageView back1;
+    private ImageView tap;
     private ConstraintLayout cl;
+    private boolean flag = true;
+    public boolean onTouchEvent(MotionEvent event) {
+        flag = false;
+        Intent questionIntent = new Intent(intro.this, MainActivity.class);
+        startActivity(questionIntent);
+        return super.onTouchEvent(event);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,16 +48,19 @@ public class intro extends Activity {
         logo1.startAnimation(lo1);
         logo2.startAnimation(lo2);
         logo3.startAnimation(lo3);
-            new CountDownTimer(3500, 500) {
-                @Override
-                public void onTick(long l) {
+                new CountDownTimer(3500, 500) {
+                    @Override
+                    public void onTick(long l) {
 
-                }
-                @Override
-                public void onFinish() {
-                    Intent questionIntent = new Intent(intro.this, MainActivity.class);
-                    startActivity(questionIntent);
-                }
-            }.start();
+                    }
+                    @Override
+                    public void onFinish() {
+                        if (flag == true)
+                        {
+                            Intent questionIntent = new Intent(intro.this, MainActivity.class);
+                            startActivity(questionIntent);
+                        }
+                    }
+                }.start();
     }
 }
--- app/src/main/java/com/example/cp/light.java
+++ app/src/main/java/com/example/cp/light.java
@@ -3,15 +3,23 @@ package com.example.cp;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+
+import java.util.Locale;
 
 public class light extends Activity {
     private SensorManager sensorManager;
@@ -19,6 +27,9 @@ public class light extends Activity {
     private SensorEventListener sensorEventListener;
     private TextView textView;
     private ImageView imageView;
+    private ImageView norma;
+    private ImageView qes;
+    public boolean flag = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +37,31 @@ public class light extends Activity {
         setContentView(R.layout.light);
         textView = (TextView) findViewById(R.id.ttt);
         imageView = (ImageView)findViewById(R.id.imageView4);
+        qes = (ImageView)findViewById(R.id.imQ);
+        norma = (ImageView)findViewById(R.id.norm);
+        Animation alp = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        qes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                qes.startAnimation(alp);
+                new CountDownTimer(500, 500) {
+                    @Override
+                    public void onTick(long l) {
+                    }
+                    @Override
+                    public void onFinish() {
+                        textView.setVisibility(View.GONE);
+                        imageView.setVisibility(View.GONE);
+                        norma.setVisibility(View.VISIBLE);
+                        qes.setVisibility(View.GONE);
+                        flag = true;
+                    }
+                }.start();
+            }
+        });
+       // String lang = Locale.getDefault().getLanguage();
+       // if(lang.equals("ru"))
+       //     imageView.setBackground(getResources().getDrawable(R.drawable.norma));
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         if (sensorManager != null)
         {
@@ -38,9 +74,9 @@ public class light extends Activity {
                 if (event.sensor.getType() == Sensor.TYPE_LIGHT)
                 {
                     textView.setText("" + (int)event.values[0] + " " + getResources().getString(R.string.lx));
-                    if ((int)event.values[0] <= 1275)
+                    if ((int)event.values[0] <= 600)
                         imageView.setImageAlpha((int)event.values[0]/5);
-                    if ((int)event.values[0] > 1275)
+                    if ((int)event.values[0] > 600)
                         imageView.setImageAlpha(255);
                 }
             }
@@ -53,8 +89,18 @@ public class light extends Activity {
     }
     @Override
     public void onBackPressed() {
-        Intent questionIntent = new Intent(light.this, MainActivity.class);
-        startActivity(questionIntent);
+        if (flag == false) {
+            Intent questionIntent = new Intent(light.this, MainActivity.class);
+            startActivity(questionIntent);
+        }
+        if (flag == true)
+        {
+            textView.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.VISIBLE);
+            norma.setVisibility(View.GONE);
+            qes.setVisibility(View.VISIBLE);
+            flag = false;
+        }
     }
     @Override
     protected void onResume() {
